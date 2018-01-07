fbe92888f1d2fe80bc9b3520607808a6e7e4714b
Dani Philipson <Daniela_Philipson@student.hks.harvard.edu>
2018-01-07 14:35:49 -0500

    add print functions

--- calculator.py
+++ calculator.py
@@ -8,4 +8,13 @@ def divide(a,b):
 	return a / b
 print "I'm going use the calculator functions to multiply 5 and 6"
 x = multiply(5,6)
-print x				
+print x	
+print "I'm going to use the calculator functions to divide 30 and 5"
+y = multiply(30,5)
+print y
+print "I'm going to use the calculator functions to add 12 and 15"
+z = add(12,15)
+print z
+print "I'm going to use the calculator functions to subtract 12 and 15"
+w = subtract(12,15)
+print w			
