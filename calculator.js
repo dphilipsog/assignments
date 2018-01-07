bf9e5c12441033a258843df639c7603c8aaaa32c
Dani Philipson <Daniela_Philipson@student.hks.harvard.edu>
2018-01-07 14:49:08 -0500

    add fuction consoles

--- calculator.js
+++ calculator.js
@@ -1,12 +1,24 @@
-def multiply(a,b) {
+function multiply(a,b) {
 	return (a*b)
 }
-def add(a,b) {
+function add(a,b) {
 	return(a+b)
 }
-def subtract(a,b) {
+function subtract(a,b) {
 	return(a-b)
 }
-def divide(a,b) {
+function divide(a,b) {
 	return(a/b)
 }
+console.log("I'm going use the calculator functions to multiply 5 and 6")
+var x = multiply(5,6)
+console.log(x)
+console.log("I' going to use the calculator functions to add 12 and 15")
+var y = add(12,15)
+console.log(y)
+console.log("I'm goint to use the calculator functions to subtract 12 and 15")
+var z = subtract(12,15)
+console.log(z)
+console.log("I'm going to use the calculator functions to divide 30 and 6")
+var w = divide(30,6)
+console.log(w)
